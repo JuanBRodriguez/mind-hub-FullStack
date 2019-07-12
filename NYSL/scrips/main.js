f106a5f92d4affcf71cd1951291754fabb9fddd0
Juan Rodriguez <jbcrodriguezsud@gmail.com>
2019-07-11 21:22:06 -0300

    implementando scrollspy personal xD

---
+++ NYSL/scrips/main.js
@@ -0,0 +1,14 @@
+$(".find-loc").click(function () {
+    console.log("holaaa")
+    $("#droploc").click();
+
+    /*if(contenido.css("display")=="none"){ //open		
+       contenido.slideDown(250);			
+       $(this).addClass("open");
+    }
+    else{ //close		
+       contenido.slideUp(250);
+       $(this).removeClass("open");	
+   }*/
+
+});
