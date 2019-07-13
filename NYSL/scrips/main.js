a2e75f51c913a386d14bbd1b9d2d00ab17c5cd2c
Juan <jbcrodriguezsud@mail.com>
2019-07-13 01:44:31 -0300

    app completa task 2

--- NYSL/scrips/main.js
+++ NYSL/scrips/main.js
@@ -1,14 +1,99 @@
-$(".find-loc").click(function () {
-    console.log("holaaa")
-    $("#droploc").click();
+$("button").click(function () {
+   let nameclass = this.className;
+   console.log(nameclass);
+   // options info
+   if (nameclass.includes("find-opt-cal")) { //open calendar
+      $("#drop-cal").collapse('show');
+   }
+   if (nameclass.includes("find-opt-tea")) { //open teams
+      $("#drop-tea").collapse('show');
+   }
+   if (nameclass.includes("find-opt-loc")) { //open locations
+      $("#drop-loc").collapse('show');
+   }
+});
 
-    /*if(contenido.css("display")=="none"){ //open		
-       contenido.slideDown(250);			
-       $(this).addClass("open");
-    }
-    else{ //close		
-       contenido.slideUp(250);
-       $(this).removeClass("open");	
-   }*/
+$("a").click(function () {
+   let nameclass = this.className;
+   console.log(nameclass);
+   
+   // switch (key) {
+   //    case value:
+         
+   //       break;
+   
+   //    default:
+   //       break;
+   // }
+   
+   // nav bar
+   if (nameclass.includes("nav-item")) { //drop nav al seleccionar
+      $("#dropnav").collapse('hide');
+   }
+   if (nameclass.includes("find-nav-hom")) { //open home
+      $("#drop-hom").collapse('show');
+   }
+   if (nameclass.includes("find-nav-abo")) { //open about
+      $("#drop-abo").collapse('show');
+   }
+   if (nameclass.includes("find-nav-inf")) { //open inf
+      $("#drop-inf").collapse('show');
+   }
+   if (nameclass.includes("find-nav-rul")) { //open rul
+      $("#drop-rul").collapse('show');
+   }
+   if (nameclass.includes("find-nav-con")) { //open con
+      $("#drop-con").collapse('show');
+   }
 
+   // locations
+   if (nameclass == "find-loc-aj") { //open aj katzenmaier
+      $("#drop-loc").collapse('show');
+      $("#dropaj").collapse('show');
+   }
+   if (nameclass == "find-loc-gre") { //open gre
+      $("#drop-loc").collapse('show');
+      $("#dropgre").collapse('show');
+   }
+   if (nameclass == "find-loc-mar") { //open mar
+      $("#drop-loc").collapse('show');
+      $("#dropmar").collapse('show');
+   }
+   if (nameclass == "find-loc-how") { //open how
+      $("#drop-loc").collapse('show');
+      $("#drophow").collapse('show');
+   }
+   if (nameclass == "find-loc-nor") { //open nor
+      $("#drop-loc").collapse('show');
+      $("#dropnor").collapse('show');
+   }
+   if (nameclass == "find-loc-sou") { //open sou
+      $("#drop-loc").collapse('show');
+      $("#dropsou").collapse('show');
+   }
+   // teams
+   if (nameclass == "find-team-u1") { //open u1
+      $("#drop-tea").collapse('show');
+      $("#dropu1").collapse('show');
+   }
+   if (nameclass == "find-team-u2") { //open u2
+      $("#drop-tea").collapse('show');
+      $("#dropu2").collapse('show');
+   }
+   if (nameclass == "find-team-u3") { //open u3
+      $("#drop-tea").collapse('show');
+      $("#dropu3").collapse('show');
+   }
+   if (nameclass == "find-team-u4") { //open u4
+      $("#drop-tea").collapse('show');
+      $("#dropu4").collapse('show');
+   }
+   if (nameclass == "find-team-u5") { //open u5
+      $("#drop-tea").collapse('show');
+      $("#dropu5").collapse('show');
+   }
+   if (nameclass == "find-team-u6") { //open u6
+      $("#drop-tea").collapse('show');
+      $("#dropu6").collapse('show');
+   }
 });
